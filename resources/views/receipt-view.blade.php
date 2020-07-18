8225e6f80732c85e525b0cfe81ccda4602cf87ef
Tru Hoai Phuong <deli4ne1@gmail.com>
2020-07-18 17:28:23 +0700

    fix receipt-view

--- resources/views/receipt-view.blade.php
+++ resources/views/receipt-view.blade.php
@@ -1,6 +1,6 @@
 @extends('master')
 @section('head')
-<title>KING | Xem phiếu thu #{{$receipt->number}}</title>
+<title>DELI | Xem phiếu thu #{{$receipt->id}}</title>
 <link rel="stylesheet" href="{{secure_asset('plugins/datatables/dataTables.bootstrap4.css')}}">
 <link rel="stylesheet" href="{{secure_asset('plugins/iCheck/square/blue.css')}}">
 @stop
@@ -35,7 +35,7 @@
               <div class="col-12">
                 <h4>
                   <i class="fa fa-globe"></i>  <b>PHIẾU THU</b>
-                  <small class="float-right"><b>SỐ PHIẾU #{{ $receipt -> number }}</b></small>
+                  <small class="float-right"><b>SỐ PHIẾU #{{$receipt->id}}</b></small>
                 </h4>
               </div>
               <!-- /.col -->
@@ -45,10 +45,10 @@
               <div class="col-md-6 invoice-col">
                 <u>Thông tin phiếu thu:</u>
                 <address>
+                  <b>Mã KH:</b> {{ $receipt->client->id }}<br>
                   <strong class="text-uppercase"><a href="{{route('staff.client.view.get', ['client_id'=>$receipt->client->id])}}">{{$receipt->client->name}}</a></strong><br>
                   <b>Số điện thoại: </b><a href="tel:{{$receipt->client->sdt}}">{{PhoneFormat($receipt->client->phone)}}</a><br>
                   <b>Ngày sinh:</b> {{ date("d/m/Y", strtotime($receipt->client->birthday)) }}<br>
-                  <b>Mã khách hàng:</b> {{ $receipt->client->id }}<br>
                   <b>Ngày lập phiếu:</b> {{ $receipt->created_at->timezone('Asia/Ho_Chi_Minh')->format("d/m/Y") }}<br>
                   <b>Nhân viên lập:</b> {{ $receipt->staff->name }}
                 </address>
@@ -71,17 +71,17 @@
               <div class="col-12 table-responsive">
                 <table class="table table-striped table table-bordered">
                   <thead>
-                    <tr>
+                    <tr class="text-center">
                       <th>STT</th>
-                      <th>Dịch vụ</th>
-                      <th>Thành tiền</th>
+                      <th>DỊCH VỤ</th>
+                      <th>THÀNH TIỀN</th>
                     </tr>
                   </thead>
                   <tbody>
                     <tr>
-                      <td class="text-uppercase">1</td>
+                      <td class="text-center">1</td>
                       <td class="text-uppercase">{{ $receipt -> content }}</td>
-                      <td class="text-uppercase">{{ number_format($receipt->amount,0,",",".") }}</td>
+                      <td class="text-right">{{ number_format($receipt->amount,0,",",".") }}</td>
                     </tr>
                   </tbody>
                 </table>
