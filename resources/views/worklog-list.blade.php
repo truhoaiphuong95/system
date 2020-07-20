157e7dcd48c0a6b182ebf03d677d960f1c8c3790
Tru Hoai Phuong <deli4ne1@gmail.com>
2020-07-20 15:36:22 +0700

    show worklog

--- resources/views/worklog-list.blade.php
+++ resources/views/worklog-list.blade.php
@@ -55,7 +55,7 @@
                         <td class="text-center">{{date('d/m/Y',strtotime($worklog->date))}}</td>
                         <td class="text-center">{{$session[$worklog->session]}}</td>
                         <td>{{$worklog->staff->name}}</td>
-                        <td>@if($worklog->staff->id == UserInfo()->id) <a href="{{route('staff.worklog.edit.get', ['worklog_id' => $worklog->id])}}" style="float: right;"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a> @endif{!!$worklog->staff->content!!}</td>
+                        <td>@if($worklog->staff->id == UserInfo()->id) <a href="{{route('staff.worklog.edit.get', ['worklog_id' => $worklog->id])}}" style="float: right;"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a> @endif @if(UserInfo()->isManager()) {!!$worklog->content!!} @endif</td>
                       </tr>
                       @endforeach
                     </tbody>
