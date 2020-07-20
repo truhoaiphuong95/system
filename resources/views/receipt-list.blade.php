debd5eb9f3e8e44f84fce5eff6999701380b3e31
Tru Hoai Phuong <deli4ne1@gmail.com>
2020-07-20 10:15:14 +0700

    fix: receipt

--- resources/views/receipt-list.blade.php
+++ resources/views/receipt-list.blade.php
@@ -66,8 +66,6 @@
                         <span class="sr-only">Toggle Dropdown</span>
                       </button>
                       <div class="dropdown-menu" role="menu">
-                        <!--<a class="dropdown-item" href="{{route('staff.receipt.printpos.get', ['receipt_id' => $data->id])}}" target="_blank">In máy POS</a>
-                        <a class="dropdown-item" href="{{route('staff.receipt.printinternal.get', ['receipt_id' => $data->id])}}" target="_blank">In phiếu dán</a>-->
                         <a class="dropdown-item" href="{{route('staff.receipt.print.get', ['receipt_id' => $data->id])}}" target="_blank">In biên nhận</a>
                       </div>
                     </div>
