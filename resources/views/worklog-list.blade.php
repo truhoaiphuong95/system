55b4396c535ba3c63c8413524669da86bde24abb
Tru Hoai Phuong <deli4ne1@gmail.com>
2020-07-20 16:50:47 +0700

    fix: worklog-list

--- resources/views/worklog-list.blade.php
+++ resources/views/worklog-list.blade.php
@@ -47,7 +47,7 @@
                         <th style="width: 200px;">BUỔI</th>
                         <th style="width: 200px;">NGƯỜI BÁO CÁO</th>
                         <th>NỘI DUNG</th>
-                        <th></th>
+                        <th style="width: 50px;">HÀNH ĐỘNG</th>
                       </tr>
                     </thead>
                     <tbody>
