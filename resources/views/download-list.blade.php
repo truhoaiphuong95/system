7d138ee49f299da2d271e0f9c039aca7a62b25b9
Tru Hoai Phuong <deli4ne1@gmail.com>
2020-07-20 13:44:59 +0700

    add all

--- resources/views/download-list.blade.php
+++ resources/views/download-list.blade.php
@@ -1,6 +1,6 @@
 @extends('master')
 @section('head')
-<title>DELI | Danh sách phần mềm</title>
+<title>DELI | Danh sách tổng hợp tài liệu</title>
 <link rel="stylesheet" href="{{secure_asset('plugins/datatables/dataTables.bootstrap4.css')}}">
 @stop
 @section('main')
@@ -11,12 +11,12 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>THƯ VIỆN TÀI NGUYÊN</h1>
+            <h1>DANH SÁCH TỔNG HỢP TÀI LIỆU</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Trang chủ</a></li>
-              <li class="breadcrumb-item active">Tải về</li>
+              <li class="breadcrumb-item active">Danh sách tổng hợp tài liệu</li>
             </ol>
           </div>
         </div>
@@ -37,7 +37,7 @@
         <div class="col-12">
           <div class="card">
             <div class="card-header">
-              <h3 class="card-title">Tài nguyên có thể tải</h3>
+              <h3 class="card-title">DANH SÁCH TỔNG HỢP TÀI LIỆU</h3>
             </div>
             <!-- /.card-header -->
             <div class="card-body">
